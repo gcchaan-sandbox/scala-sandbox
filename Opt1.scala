5ad6d0ee2415dc1f55301737b0846995c919f045
gcchaan <2ndtofind@gmail.com>
2015-10-13 22:47:51 +0900

    more been scala

--- Opt1.scala
+++ Opt1.scala
@@ -1,16 +1,14 @@
 object Opt1 {
-  def main (args: Array[String]): Unit ={
-    val (a, b, c, d, e, f) = (1,2,3,4,5,6)
-//    val (a, b, c, d, e, f) = (104,35,77,36,29,6)
-//    val (a, b, c, d, e, f) = (1,2,3,1,2,1)
-    val mes = if ((b * d - a * e) != 0) {
-      val x = (b * f - c * e).toFloat / (b * d - a * e).toFloat
-//println(b * f - c * e)
-//println(b * d - a * e)
-      val y = (a * f - c * d).toFloat / (a * e - b * d).toFloat
-      "x:" + x.toString() + ", y:" + y.toString
-    } else {
+  def main(args: Array[String]) = {
+//    val (a, b, c, d, e, f) = (1, 2, 3, 4, 5, 6)
+    val (a, b, c, d, e, f) = (104, 35, 77, 36, 29, 6)
+//    val (a, b, c, d, e, f) = (1, 2, 3, 1, 2, 1)
+    val mes = if ((b * d - a * e) == 0) {
       "解なし"
+    } else {
+      val x = (b * f - c * e).toFloat / (b * d - a * e).toFloat
+      val y = (a * f - c * d).toFloat / (a * e - b * d).toFloat
+      s"x=${x}, y=${y}"
     }
     println(mes)
   }
