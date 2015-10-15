07eda20574507fd683fa4236d8628a5c75738d39
gcchaan <2ndtofind@gmail.com>
2015-10-15 21:23:53 +0900

    simplify

--- Opt1.scala
+++ Opt1.scala
@@ -1,15 +1,10 @@
-object Opt1 {
-  def main(args: Array[String]) = {
-//    val (a, b, c, d, e, f) = (1, 2, 3, 4, 5, 6)
-    val (a, b, c, d, e, f) = (104, 35, 77, 36, 29, 6)
-//    val (a, b, c, d, e, f) = (1, 2, 3, 1, 2, 1)
-    val mes = if ((b * d - a * e) == 0) {
-      "解なし"
-    } else {
-      val x = (b * f - c * e).toFloat / (b * d - a * e).toFloat
-      val y = (a * f - c * d).toFloat / (a * e - b * d).toFloat
-      s"x=${x}, y=${y}"
-    }
-    println(mes)
+def calc(a: Float, b: Float,c: Float, d: Float, e: Float, f: Float) = {
+  if ((b * d - a * e) == 0) {
+    "解なし"
+  } else {
+    val x = (b * f - c * e) / (b * d - a * e)
+    val y = (a * f - c * d) / (a * e - b * d)
+    s"x=${x}, y=${y}"
   }
 }
+//println(calc(104, 35, 77, 36, 29, 6))
